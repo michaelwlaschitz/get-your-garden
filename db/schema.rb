6d5cc5110dd4330ed10a1d4e626e829f72c46b34
Joyce Cakmak <joyce.cakmak@gmail.com>
2020-03-16 18:04:58 +0100

    garden

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_162856) do
+ActiveRecord::Schema.define(version: 2020_03_16_170010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "gardens", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "location"
+    t.integer "price"
+    t.integer "capacity"
+    t.bigint "user_id"
+    t.boolean "barbecue"
+    t.boolean "pool"
+    t.boolean "green_area"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_gardens_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +44,5 @@ ActiveRecord::Schema.define(version: 2020_03_16_162856) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "gardens", "users"
 end
