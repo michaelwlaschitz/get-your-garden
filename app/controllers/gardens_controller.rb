5c503ab819ab09e9a3e41705f186aa0af5d67ea3
Laura Helmert <laura.helmert.berlin@gmail.com>
2020-03-17 12:06:42 +0100

    active storage installed

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -3,9 +3,15 @@ class GardensController < ApplicationController
   end
 
   def new
+    @garden = Garden.new
   end
 
   def create
+    @user = current_user
+    @garden = Garden.new(garden_params)
+    @user = @garden.user
+    @garden.save
+   # re_direct to garden_path(@garden)
   end
 
   def show
@@ -16,4 +22,12 @@ class GardensController < ApplicationController
 
   def update
   end
+
+  private
+
+  def garden_params
+    params.require(:garden).permit(:title, :description, :location, :price, :capacity, :barbecue,
+   :pool, :green_area, photos: [])
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'gardens/index'
-  get 'gardens/new'
-  get 'gardens/create'
-  get 'gardens/show'
-  get 'gardens/edit'
-  get 'gardens/update'
   devise_for :users
   root to: 'pages#home'
-  resources :garden, only: [:index, :create, :new]
+  resources :gardens, only: [:index, :create, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
