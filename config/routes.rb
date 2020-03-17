da3ace593b0176123d31e9a005b56af4db52b214
Michael Wlaschitz <michael.wlaschitz@gmail.com>
2020-03-17 12:51:54 +0100

    lala

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'gardens/index'
-  get 'gardens/new'
-  get 'gardens/create'
-  get 'gardens/show'
-  get 'gardens/edit'
-  get 'gardens/update'
   devise_for :users
   root to: 'pages#home'
   resources :garden, only: [:index, :create, :new]
