ed16ee94c9861c2975a3280aa53a3309748e8fc6
Joyce Cakmak <joyce.cakmak@gmail.com>
2020-03-19 12:43:07 +0100

    model and controller with index and create

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,31 @@
+class BookingsController < ApplicationController
+
+  def index
+    @bookings = current_user.bookings
+  end
+
+  def create
+    @user = current_user #grabing the current user (helper method given be devise gem)
+    @booking = Booking.new(booking_params) # creating a booking instance passing the strong params of the booking
+    @garden = Garden.find(params[:garden_id]) #find the gardin id in the url
+    @booking.garden = @garden #assigning the garden from the url as the garden of the booking we are creating
+    @booking.user = @user # assigning the current user as a user of the booking we are creating
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def accept
+  end
+
+  def reject
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :start_time, :end_time, :status)
+  end
+end
--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -18,6 +18,7 @@ class GardensController < ApplicationController
 
   def show
     @garden = Garden.find(params[:id])
+    @booking = Booking.new
   end
 
   def create
