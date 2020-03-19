d03b19a639fa2731d88e2eb12e8a304c409be567
Michael Wlaschitz <michael.wlaschitz@gmail.com>
2020-03-19 15:16:00 +0100

    booking basics done

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -32,7 +32,6 @@ class GardensController < ApplicationController
   end
 
 
-
   def edit
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "/test_bookings", to: "pages#test_bookings"
   resources :gardens, only: [:index, :create, :new, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
